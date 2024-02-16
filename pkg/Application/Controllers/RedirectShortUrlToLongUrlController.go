3906bec8c55ce8e16a407299a5234ce013cca175
João de Souza <joaogbdesouza@gmail.com>
2024-02-15 23:20:41 -0300

    checking if short url exists before redirect

--- pkg/Application/Controllers/RedirectShortUrlToLongUrlController.go
+++ pkg/Application/Controllers/RedirectShortUrlToLongUrlController.go
@@ -17,14 +17,22 @@ func (controller *RedirectShortUrlToLongUrlController) Execute(w http.ResponseWr
 	mainRepo := repositories.Repository{}
 	db := mainRepo.GetDbInstance()
 
-	url := models.Url{
-		Short_url: urlParameters["short_url"],
+	url := models.Url{}
+	result := db.First(&url, "short_url = ?", urlParameters["short_url"])
+
+	if result.RowsAffected == 0 {
+		w.WriteHeader(http.StatusNotFound)
+
+		w.Write(response(false, "Url not found", nil))
+		return
 	}
-	db.Find(&url)
 
 	if url.Long_url == "" {
 		panic("long url not defined")
 	}
+
+	url.Is_used = true
+	db.Save(url)
 
 	http.Redirect(w, req, url.Long_url, http.StatusSeeOther)
 }
