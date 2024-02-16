3ae517a5d4e32778f359d37242b88b6da9a355cd
João de Souza <joaogbdesouza@gmail.com>
2024-02-15 23:00:18 -0300

    generating random string + validation to avoid duplicate during runtime

--- pkg/Application/Controllers/CreateShortUrlController.go
+++ pkg/Application/Controllers/CreateShortUrlController.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-	"fmt"
+	"math/rand"
 	"net/http"
 	"time"
 
@@ -18,22 +18,45 @@ func (controller *CreateShortUrlController) Execute(w http.ResponseWriter, req *
 
 	long_url := req.PostFormValue("long_url")
 
+	randomString := RandomString(7)
+
 	url := models.Url{
 		Long_url:    long_url,
-		Short_url:   "testing123",
-		Expire_date: time.Now(),
+		Short_url:   randomString,
+		Expire_date: time.Now().AddDate(100, 0, 0),
 	}
 
 	mainRepo := repositories.Repository{}
 	db := mainRepo.GetDbInstance()
 
+	// checking if short url already exists
+	result := db.First(&models.Url{}, "short_url = ?", randomString)
+
+	if result.RowsAffected > 0 {
+		for result.RowsAffected > 0 {
+			randomString = RandomString(7)
+			result = db.First(&models.Url{}, "short_url = ?", randomString)
+			url.Short_url = randomString
+		}
+	}
+
 	db.Create(&url)
 
-	fmt.Println(url)
-
 	w.Header().Set("Content-Type", "application/json")
-	response := response(true, "Success!", nil)
+	response := response(true, "Success!", map[string]string{
+		"shor_url": url.Short_url,
+	})
 
 	w.Write(response)
 
+}
+
+func RandomString(size int) string {
+	const alphaNumerical = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+
+	b := make([]byte, size)
+	for i := range b {
+		b[i] = alphaNumerical[rand.Intn(len(alphaNumerical))]
+	}
+	return string(b)
 }
