f7e67abb1a8428296118e82a41e7cb07310ae0e1
João de Souza <joaogbdesouza@gmail.com>
2024-02-23 14:32:28 -0300

    refactoring the redirect and caching and using results

--- pkg/Application/Controllers/RedirectShortUrlToLongUrlController.go
+++ pkg/Application/Controllers/RedirectShortUrlToLongUrlController.go
@@ -1,24 +1,44 @@
 package controllers
 
 import (
+	"fmt"
+	"log"
 	"net/http"
 	"time"
 
 	models "github.com/gabszero/url-shortener/pkg/Infrastructure/Models"
 	repositories "github.com/gabszero/url-shortener/pkg/Infrastructure/Repositories"
+	services "github.com/gabszero/url-shortener/pkg/Services"
 	"github.com/gorilla/mux"
 )
 
 type RedirectShortUrlToLongUrlController struct {
+	urlService services.UrlService
 }
 
 func (controller *RedirectShortUrlToLongUrlController) Execute(w http.ResponseWriter, req *http.Request) {
 	urlParameters := mux.Vars(req)
 
 	mainRepo := repositories.Repository{}
-	db := mainRepo.GetDbInstance(1)
+	shard := controller.urlService.GetShard(string(urlParameters["short_url"][0]))
+	db := mainRepo.GetDbInstance(shard)
+	redisInstance := mainRepo.GetRedisInstance()
 
 	url := models.Url{}
+	long_url, noValueFoundError := redisInstance.Get(req.Context(), "BbpBaQk").Result()
+	fmt.Println(long_url)
+	fmt.Println(urlParameters["short_url"])
+	fmt.Println(noValueFoundError)
+	if noValueFoundError != nil {
+		long_url = ""
+	}
+
+	if long_url != "" {
+		log.Println("Found url in cache")
+		http.Redirect(w, req, long_url, http.StatusSeeOther)
+		return
+	}
+
 	result := db.First(&url, "short_url = ? and expire_date > ?", urlParameters["short_url"], time.Now())
 
 	if result.RowsAffected == 0 {
@@ -26,6 +46,11 @@ func (controller *RedirectShortUrlToLongUrlController) Execute(w http.ResponseWr
 
 		w.Write(response(false, "Url not found", nil))
 		return
+	}
+
+	err := redisInstance.Set(req.Context(), url.Short_url, url.Long_url, 0).Err()
+	if err != nil {
+		panic(err)
 	}
 
 	url.Is_used = true
